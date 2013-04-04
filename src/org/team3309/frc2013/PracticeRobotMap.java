90dfd626c7f1b7909c75ba4e04062338ffef091f
Vinnie Magro <vmagro@socaldevs.com>
2013-04-04 13:28:08 -0700

    Fancy auto-switch from competition to practice robot map

--- src/org/team3309/frc2013/PracticeRobotMap.java
+++ src/org/team3309/frc2013/PracticeRobotMap.java
@@ -8,7 +8,7 @@ package org.team3309.frc2013;
  *
  * @author friarbots
  */
-public class CompetitionRobotMap {
+public class PracticeRobotMap {
     
     public static final int COMPRESSOR_RELAY    = 1;
     public static final int PRESSURE_SWITCH     = 14;
@@ -17,19 +17,19 @@ public class CompetitionRobotMap {
     public static final int DRIVE_LEFT_2    = 2;
     public static final int DRIVE_RIGHT_1   = 3;
     public static final int DRIVE_RIGHT_2   = 4;
-    public static final int DRIVE_ENCODER_LEFT_A    = 12;
-    public static final int DRIVE_ENCODER_LEFT_B    = 13;
+    public static final int DRIVE_ENCODER_LEFT_A    = 2;
+    public static final int DRIVE_ENCODER_LEFT_B    = 10;
     public static final int DRIVE_ENCODER_RIGHT_A   = 1;
-    public static final int DRIVE_ENCODER_RIGHT_B   = 2;
-    public static final int DRIVE_GYRO = 1;
+    public static final int DRIVE_ENCODER_RIGHT_B   = 11;
+    public static final int DRIVE_GYRO  = 1;
     
-    public static final int DRIVE_SHIFTER_FORWARD       = 6;
-    public static final int DRIVE_SHIFTER_REVERSE       = 7;
-    public static final int DRIVE_SHIFTER_ENGAGE_PTO    = 5;
+    public static final int DRIVE_SHIFTER_FORWARD       = 7;
+    public static final int DRIVE_SHIFTER_REVERSE       = 2;
+    public static final int DRIVE_SHIFTER_ENGAGE_PTO    = 6;
     
     public static final int SHOOTER_MOTOR   = 6;
-    public static final int SHOOTER_LOADER  = 3;
-    public static final int SHOOTER_TILTER  = 2;
+    public static final int SHOOTER_LOADER  = 5;
+    public static final int SHOOTER_TILTER  = 3;
     public static final int SHOOTER_PHOTOSENSOR_CHANNEL = 9;
     
     
--- src/org/team3309/frc2013/RobotMap.java
+++ src/org/team3309/frc2013/RobotMap.java
@@ -10,34 +10,103 @@ package org.team3309.frc2013;
  */
 public class RobotMap {
     
-    public static final int COMPRESSOR_RELAY    = 1;
-    public static final int PRESSURE_SWITCH     = 14;
+    private static final boolean isCompBot = true;
+    
+    static{
+        System.out.println("Competition robot status: "+isCompBot);
+        if(isCompBot){
+            COMPRESSOR_RELAY = CompetitionRobotMap.COMPRESSOR_RELAY;
+            PRESSURE_SWITCH = CompetitionRobotMap.PRESSURE_SWITCH;
+            
+            DRIVE_LEFT_1 = CompetitionRobotMap.DRIVE_LEFT_1;
+            DRIVE_LEFT_2 = CompetitionRobotMap.DRIVE_LEFT_2;
+            DRIVE_RIGHT_1 = CompetitionRobotMap.DRIVE_RIGHT_1;
+            DRIVE_RIGHT_2 = CompetitionRobotMap.DRIVE_RIGHT_2;
+            DRIVE_ENCODER_LEFT_A = CompetitionRobotMap.DRIVE_ENCODER_LEFT_A;
+            DRIVE_ENCODER_LEFT_B = CompetitionRobotMap.DRIVE_ENCODER_LEFT_B;
+            DRIVE_ENCODER_RIGHT_A = CompetitionRobotMap.DRIVE_ENCODER_RIGHT_A;
+            DRIVE_ENCODER_RIGHT_B = CompetitionRobotMap.DRIVE_ENCODER_RIGHT_B;
+            DRIVE_GYRO = CompetitionRobotMap.DRIVE_GYRO;
+            
+            DRIVE_SHIFTER_FORWARD = CompetitionRobotMap.DRIVE_SHIFTER_FORWARD;
+            DRIVE_SHIFTER_REVERSE = CompetitionRobotMap.DRIVE_SHIFTER_REVERSE;
+            DRIVE_SHIFTER_ENGAGE_PTO = CompetitionRobotMap.DRIVE_SHIFTER_ENGAGE_PTO;
+            
+            SHOOTER_MOTOR = CompetitionRobotMap.SHOOTER_MOTOR;
+            SHOOTER_LOADER = CompetitionRobotMap.SHOOTER_LOADER;
+            SHOOTER_TILTER = CompetitionRobotMap.SHOOTER_TILTER;
+            SHOOTER_PHOTOSENSOR_CHANNEL = CompetitionRobotMap.SHOOTER_PHOTOSENSOR_CHANNEL;
+            
+            CLIMBER_TIPPER_FORWARD = CompetitionRobotMap.CLIMBER_TIPPER_FORWARD;
+            CLIMBER_TIPPER_REVERSE = CompetitionRobotMap.CLIMBER_TIPPER_REVERSE;
+            CLIMBER_TOP_LIMIT = CompetitionRobotMap.CLIMBER_TOP_LIMIT;
+            CLIMBER_BOTTOM_LIMIT = CompetitionRobotMap.CLIMBER_BOTTOM_LIMIT;
+            CLIMBER_LOCK = CompetitionRobotMap.CLIMBER_LOCK;
+            
+            DUMPER = CompetitionRobotMap.DUMPER;
+        }
+        
+        else{
+            COMPRESSOR_RELAY = PracticeRobotMap.COMPRESSOR_RELAY;
+            PRESSURE_SWITCH = PracticeRobotMap.PRESSURE_SWITCH;
+            
+            DRIVE_LEFT_1 = PracticeRobotMap.DRIVE_LEFT_1;
+            DRIVE_LEFT_2 = PracticeRobotMap.DRIVE_LEFT_2;
+            DRIVE_RIGHT_1 = PracticeRobotMap.DRIVE_RIGHT_1;
+            DRIVE_RIGHT_2 = PracticeRobotMap.DRIVE_RIGHT_2;
+            DRIVE_ENCODER_LEFT_A = PracticeRobotMap.DRIVE_ENCODER_LEFT_A;
+            DRIVE_ENCODER_LEFT_B = PracticeRobotMap.DRIVE_ENCODER_LEFT_B;
+            DRIVE_ENCODER_RIGHT_A = PracticeRobotMap.DRIVE_ENCODER_RIGHT_A;
+            DRIVE_ENCODER_RIGHT_B = PracticeRobotMap.DRIVE_ENCODER_RIGHT_B;
+            DRIVE_GYRO = PracticeRobotMap.DRIVE_GYRO;
+            
+            DRIVE_SHIFTER_FORWARD = PracticeRobotMap.DRIVE_SHIFTER_FORWARD;
+            DRIVE_SHIFTER_REVERSE = PracticeRobotMap.DRIVE_SHIFTER_REVERSE;
+            DRIVE_SHIFTER_ENGAGE_PTO = PracticeRobotMap.DRIVE_SHIFTER_ENGAGE_PTO;
+            
+            SHOOTER_MOTOR = PracticeRobotMap.SHOOTER_MOTOR;
+            SHOOTER_LOADER = PracticeRobotMap.SHOOTER_LOADER;
+            SHOOTER_TILTER = PracticeRobotMap.SHOOTER_TILTER;
+            SHOOTER_PHOTOSENSOR_CHANNEL = PracticeRobotMap.SHOOTER_PHOTOSENSOR_CHANNEL;
+            
+            CLIMBER_TIPPER_FORWARD = PracticeRobotMap.CLIMBER_TIPPER_FORWARD;
+            CLIMBER_TIPPER_REVERSE = PracticeRobotMap.CLIMBER_TIPPER_REVERSE;
+            CLIMBER_TOP_LIMIT = PracticeRobotMap.CLIMBER_TOP_LIMIT;
+            CLIMBER_BOTTOM_LIMIT = PracticeRobotMap.CLIMBER_BOTTOM_LIMIT;
+            CLIMBER_LOCK = PracticeRobotMap.CLIMBER_LOCK;
+            
+            DUMPER = PracticeRobotMap.DUMPER;
+        }
+    }
+    
+    public static int COMPRESSOR_RELAY;
+    public static int PRESSURE_SWITCH;
 
-    public static final int DRIVE_LEFT_1    = 1;
-    public static final int DRIVE_LEFT_2    = 2;
-    public static final int DRIVE_RIGHT_1   = 3;
-    public static final int DRIVE_RIGHT_2   = 4;
-    public static final int DRIVE_ENCODER_LEFT_A    = 2;
-    public static final int DRIVE_ENCODER_LEFT_B    = 10;
-    public static final int DRIVE_ENCODER_RIGHT_A   = 1;
-    public static final int DRIVE_ENCODER_RIGHT_B   = 11;
-    public static final int DRIVE_GYRO  = 1;
+    public static int DRIVE_LEFT_1;
+    public static int DRIVE_LEFT_2;
+    public static int DRIVE_RIGHT_1;
+    public static int DRIVE_RIGHT_2;
+    public static int DRIVE_ENCODER_LEFT_A;
+    public static int DRIVE_ENCODER_LEFT_B;
+    public static int DRIVE_ENCODER_RIGHT_A;
+    public static int DRIVE_ENCODER_RIGHT_B;
+    public static int DRIVE_GYRO;
     
-    public static final int DRIVE_SHIFTER_FORWARD       = 7;
-    public static final int DRIVE_SHIFTER_REVERSE       = 2;
-    public static final int DRIVE_SHIFTER_ENGAGE_PTO    = 6;
+    public static int DRIVE_SHIFTER_FORWARD;
+    public static int DRIVE_SHIFTER_REVERSE;
+    public static int DRIVE_SHIFTER_ENGAGE_PTO;
     
-    public static final int SHOOTER_MOTOR   = 6;
-    public static final int SHOOTER_LOADER  = 5;
-    public static final int SHOOTER_TILTER  = 3;
-    public static final int SHOOTER_PHOTOSENSOR_CHANNEL = 9;
+    public static int SHOOTER_MOTOR;
+    public static int SHOOTER_LOADER;
+    public static int SHOOTER_TILTER;
+    public static int SHOOTER_PHOTOSENSOR_CHANNEL;
     
+    public static int CLIMBER_TIPPER_FORWARD;
+    public static int CLIMBER_TIPPER_REVERSE;
+    public static int CLIMBER_TOP_LIMIT;
+    public static int CLIMBER_BOTTOM_LIMIT;
+    public static int CLIMBER_LOCK;
     
-    public static final int CLIMBER_TIPPER_FORWARD  = 1;
-    public static final int CLIMBER_TIPPER_REVERSE  = 8;
-    public static final int CLIMBER_TOP_LIMIT       = 5;
-    public static final int CLIMBER_BOTTOM_LIMIT    = 6;
-    public static final int CLIMBER_LOCK            = 4;
+    public static int DUMPER;
     
-    public static final int DUMPER = 5;
 }
